e63b06471c3f65808f7ff11d2d671f3ed09d3a74
jnpj <jnjermain@gmail.com>
2016-08-26 19:49:22 +0100

    style: Replace double quotes in journals.js with single as per style guide

--- js/collections/journals.js
+++ js/collections/journals.js
@@ -12,18 +12,18 @@ app.JournalList = Backbone.Collection.extend({
 	createDateString: function(date) {
 		var dateString;
 
-		var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-			"Friday", "Saturday"];
-		var months = ["January", "February", "March", "April", "May", "June",
-			"July", "August", "September", "October", "November", "December"];
+		var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+			'Friday', 'Saturday'];
+		var months = ['January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December'];
 
 		var dayOfWeek = date.getDay();
 		var dayOfMonth = date.getDate();
 		var month = date.getMonth();
 		var year = date.getFullYear();
 
-		dateString = weekdays[dayOfWeek] + ", " + months[month] + " " +
-			dayOfMonth + " " + year;
+		dateString = weekdays[dayOfWeek] + ', ' + months[month] + ' ' +
+			dayOfMonth + ' ' + year;
 
 		return dateString;
 
